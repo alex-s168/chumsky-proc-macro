adfb7974add5bcfbf2141d98cb7cd7e2510b2f76
alexander.nutz <alexander.nutz@tmconnected.com>
2025-05-27 09:31:18 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,189 @@
+use proc_macro2::{Spacing, TokenTree};
+use chumsky::{extra::ParserExtra, input::ValueInput, label::LabelError, util::MaybeRef, Parser};
+
+pub trait LikeTokenTree {
+    fn as_tok(&self) -> &TokenTree;
+}
+
+impl LikeTokenTree for TokenTree {
+    fn as_tok(&self) -> &TokenTree {
+        self
+    }
+}
+
+#[non_exhaustive]
+pub enum Expected
+{
+    StandAlonePunct(char),
+    PunctSeq(String)
+}
+
+/// A single punctuation character like `+`, `-` or `#`.
+///
+/// this does NOT match the `#` in `#+`, if this should match `#`,
+/// because this matches stand-alone punctuation only
+pub fn punct<'src, I, E>(val: char) -> impl Parser<'src, I, (), E>
+where
+    I: ValueInput<'src>,
+    I::Token: LikeTokenTree + 'src,
+    E: ParserExtra<'src, I>,
+    E::Error: LabelError<'src, I, Expected>,
+{
+    punct_impl(val, Spacing::Alone)
+}
+
+fn punct_impl<'src, I, E>(val: char, spacing: Spacing) -> impl Parser<'src, I, (), E>
+where
+    I: ValueInput<'src>,
+    I::Token: LikeTokenTree + 'src,
+    E: ParserExtra<'src, I>,
+    E::Error: LabelError<'src, I, Expected>,
+{
+    use chumsky::prelude::*;
+    any().try_map(move |x: I::Token, span| {
+        match &x.as_tok() {
+            TokenTree::Punct(p) => {
+                if p.spacing() == spacing && p.as_char() == val {
+                    Some(())
+                } else {
+                    None
+                }
+            }
+
+            _ => None
+        }.ok_or_else(|| LabelError::expected_found(
+                [Expected::StandAlonePunct(val)],
+                Some(MaybeRef::Val(x)),
+                span))
+    })
+}
+
+/// A sequence of punctuation character like `+=`, '--', or even `#<##>`.
+///
+/// when matching `#<##>`, this does not match `#<##>#`
+pub fn punct_seq<'src, I, E, S: AsRef<str>>(seq: S) -> impl Parser<'src, I, (), E>
+where
+    I: ValueInput<'src>,
+    I::Token: LikeTokenTree + 'src,
+    E: ParserExtra<'src, I>,
+    E::Error: LabelError<'src, I, Expected>,
+{
+    use chumsky::prelude::*;
+
+    let seq = seq.as_ref().to_string();
+    assert!(!seq.len() >= 2);
+
+    let seq_last = seq.chars().nth(seq.len() - 1).unwrap();
+    let seq_first = seq.chars().next().unwrap();
+
+    punct_impl(seq_first, if seq.len() == 2 { Spacing::Joint } else { Spacing::Alone })
+        .then_ignore(
+            seq.chars().skip(1).take(seq.len() - 2).map(|val| {
+                punct_impl(val, Spacing::Joint)
+                    .boxed()
+            }).reduce(|a,b| a.then_ignore(b).boxed()).unwrap_or(empty().boxed()))
+        .then_ignore(punct_impl(seq_last, Spacing::Alone))
+        .map_err_with_state(move |_,span,_| LabelError::expected_found(
+                    [Expected::PunctSeq(seq.clone())],
+                    None,
+                    span))
+        .boxed()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use quote::quote;
+    use chumsky::{extra::Err, input::Stream, prelude::*};
+
+    #[test]
+    fn test_punct() {
+        let toks = quote! { + }.into_iter();
+
+        let parser = &punct::<_, Err<Simple<_>>>('+');
+        let _v = parser.parse(Stream::from_iter(toks))
+            .into_result()
+            .unwrap();
+    }
+
+    #[test]
+    fn test_punct2() {
+        let toks = quote! { += }.into_iter();
+
+        let parser = &punct::<_, Err<Simple<_>>>('+');
+        let _v = parser.parse(Stream::from_iter(toks))
+            .into_result()
+            .unwrap_err();
+    }
+
+    #[test]
+    fn test_punct_seq() {
+        let toks = quote! { --> }.into_iter();
+
+        let parser = &punct_seq::<_, Err<Simple<_>>, _>("-->");
+        let _v = parser.parse(Stream::from_iter(toks))
+            .into_result()
+            .unwrap();
+    }
+
+    #[test]
+    fn test_punct_seq2() {
+        let toks = quote! { -> }.into_iter();
+
+        let parser = &punct_seq::<_, Err<Simple<_>>, _>("-->");
+        let _v = parser.parse(Stream::from_iter(toks))
+            .into_result()
+            .unwrap_err();
+    }
+
+    #[test]
+    fn test_punct_seq3() {
+        let toks = quote! { --># }.into_iter();
+
+        let parser = &punct_seq::<_, Err<Simple<_>>, _>("-->");
+        let _v = parser.parse(Stream::from_iter(toks))
+            .into_result()
+            .unwrap_err();
+    }
+
+    #[test]
+    fn test_punct_seq4() {
+        let toks = quote! { ---> }.into_iter();
+
+        let parser = &punct_seq::<_, Err<Simple<_>>, _>("-->");
+        let _v = parser.parse(Stream::from_iter(toks))
+            .into_result()
+            .unwrap_err();
+    }
+
+    #[test]
+    fn test_punct_seq5() {
+        let toks = quote! { -> }.into_iter();
+
+        let parser = &punct_seq::<_, Err<Simple<_>>, _>("->");
+        let _v = parser.parse(Stream::from_iter(toks))
+            .into_result()
+            .unwrap();
+    }
+
+    #[test]
+    fn test_punct_seq6() {
+        let toks = quote! { ->< }.into_iter();
+
+        let parser = &punct_seq::<_, Err<Simple<_>>, _>("->").then(punct('<'));
+        let _v = parser.parse(Stream::from_iter(toks))
+            .into_result()
+            .unwrap();
+    }
+
+    #[test]
+    fn test_punct_seq7() {
+        let toks = quote! { ---> }.into_iter();
+        println!("{:?}", toks);
+
+        let parser = &punct_seq::<_, Err<Simple<_>>, _>("--->");
+        let _v = parser.parse(Stream::from_iter(toks))
+            .into_result()
+            .unwrap();
+    }
+}
